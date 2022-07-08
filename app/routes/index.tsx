59ce22d1d1a47b5cbf2f641b2aa0a09f2d7ba2a6
Jeff Beltran <jeffbeltran@gmail.com>
2022-07-08 11:46:56 -0700

    add icons to show errors

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,12 @@
+import { GlobeAltIcon } from '@heroicons/react/outline'
+import { GlobeIcon } from '@heroicons/react/solid'
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>HERO ICONS DEBUG</h1>
+      <p>Outline <GlobeAltIcon style={{width: '1rem', height: '1rem'}}/></p>
+      <p>Solid <GlobeIcon style={{width: '1rem', height: '1rem'}}/></p>
     </div>
   );
 }
